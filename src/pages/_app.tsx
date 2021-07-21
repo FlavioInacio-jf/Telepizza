007b5cabdea011bd11c39e4d1f2c2bd92be72f70
Flávio Inácio <jflavioinacio22@gmail.com>
2021-07-21 19:21:47 -0300

    update global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=10, minimum-scale=1.0"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
