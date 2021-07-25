d829ef5ab3e59cea8c5c1a2c1807f7becd77de18
Flávio Inácio <jflavioinacio22@gmail.com>
2021-07-25 15:31:42 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -109,7 +109,7 @@ export default function Home() {
               <div className={styles.bannerIntroductionContent}>
                 <h3 className={styles.titleBanner}>Super oferta!!!</h3>
                 <p className={styles.bannerIntroductionText}>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
                 </p>
 
                 <div className={styles.bannerIntroductionFooter}>
