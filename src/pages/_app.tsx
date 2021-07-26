d4e435a9a37e26880adcdd771697c395591eedbd
Flávio Inácio <jflavioinacio22@gmail.com>
2021-07-26 17:36:52 -0300

    add map

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=10, minimum-scale=1.0"></meta>
+        <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />
       </Head>
       <Component {...pageProps} />
     </>
