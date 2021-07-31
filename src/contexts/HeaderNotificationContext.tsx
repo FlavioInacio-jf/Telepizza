0903e861d779df13e6f9aea28c3e3ec2fcd21c18
Flávio Inácio <jflavioinacio22@gmail.com>
2021-07-31 20:34:03 -0300

    update

--- src/contexts/HeaderNotificationContext.tsx
+++ src/contexts/HeaderNotificationContext.tsx
@@ -1,5 +1,7 @@
 import { createContext, ReactNode, useContext, useEffect, useState } from "react";
-import nookies from 'nookies';
+import { GetServerSidePropsContext } from 'next';
+
+import nookies, { parseCookies } from 'nookies';
 
 type NotificationProviderProps = {
   children: ReactNode;
@@ -14,15 +16,20 @@ const NotificationContext = createContext({} as NotificationContextType);
 export default function NotificationProvider( {children}: NotificationProviderProps) {
   const [isClose, setClose] = useState<Boolean>(true);
 
+
   function handleCloseNotification(state: boolean) {
     setClose(state);
     const stateString = String(isClose);
-    nookies.set(null, 'headerNotification', stateString, {
+    nookies.set(null, 'HEADER_NOTIFICATION', stateString, {
       maxAge: 30 * 24 *60 *60,
       path: '/',
     })
   }
 
+  useEffect(() => {
+    const headerNotificationState = !!Object.values(parseCookies())[0]
+    setClose(!headerNotificationState);
+  }, [])
 
   return (
     <NotificationContext.Provider value={{
@@ -37,4 +44,15 @@ export default function NotificationProvider( {children}: NotificationProviderPr
 export function useNotification() {
   const context = useContext(NotificationContext);
   return context
+}
+
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+
+  const cookies = nookies.get(context);
+  const notification = cookies.HEADER_NOTIFICATION;
+
+  return {
+     notification
+  }
 }
