e4a81dcbd72af05655e1b077e03af13120fec8d4
Flávio Inácio <jflavioinacio22@gmail.com>
2021-08-09 21:31:26 -0300

    update

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,7 @@
 import { useState, Component } from 'react';
 import Image from 'next/image';
 import { useMediaQuery } from 'react-responsive';
+import cx from 'classnames';
 
 import { FaHome, FaPizzaSlice, FaInfo, FaUsers, FaBars, FaTimes, FaWhatsapp } from 'react-icons/fa';
 
@@ -10,61 +11,55 @@ import Link from '../Link';
 import Button from '../Button';
 import styles from './Menu.module.scss';
 import HeaderNotification from '../HeaderNotification';
-import { useEffect } from 'react';
 
-export default function Menu () {
-
-
-  const isLargeDevice = useMediaQuery({
-    query: '(min-width: 992px'
-  })
-  const [isShowMenu, setShowMenu] = useState<Boolean>(false);
+export default function Menu() {
   const { isClose } = useNotification();
 
-  function handleShowMenu() {
-    setShowMenu(!isShowMenu);
+  const [isShowMenu, setShowMenu] = useState<Boolean>();
+
+  const [isMenuMobileOpen, setMenuMobileOpen] = useState<Boolean>(false);
+
+  function handleShowMenu(state: boolean) {
+    setShowMenu(state);
   }
 
-  useEffect(() =>{
-    if(isLargeDevice) {
-      setShowMenu(true);
-    }
-    else {
-      setShowMenu(false);
-    }
-    console.log(isShowMenu)
-  }, [isLargeDevice])
+  const handleMediaQueryChange = (matches) => {
+    handleShowMenu(matches);
+  }
+
+  const isLargeDevice = useMediaQuery({ minWidth: 992 }, undefined, handleMediaQueryChange);
+
+  const classNames = isMenuMobileOpen ? `${styles['menuMobileOpen']} ${styles['nav']}` : styles.nav;
 
   return (
     <>
-      { isClose && <HeaderNotification />}
+      {isClose && <HeaderNotification />}
 
-      <header id={styles.header}>
+      <header className={cx(styles.header, { menuDesktop: isShowMenu })}>
         <div className="container">
-          <button type="button" className={styles.menuBarButton} onClick={handleShowMenu}>
-            {isShowMenu ? <FaTimes /> : <FaBars />}
+          <button
+            type="button"
+            className={styles.menuBarButton}
+            onClick={() => setMenuMobileOpen(!isMenuMobileOpen)}
+          >
+            {isMenuMobileOpen ? <FaTimes /> : <FaBars />}
           </button>
-          {isShowMenu &&
-            (
-              <nav className={styles.nav} onClick={handleShowMenu}>
-                <div className={styles.wrap}>
-                  <div className={'grid ' + styles.navbar}>
-                    <Link to="/" className="current-page"><FaHome className={styles.iconDistance} />Home</Link>
-                    <Link to="/Cardapio"><FaPizzaSlice className={styles.iconDistance} />Menu</Link>
-                    <Link to="/Contact"><FaUsers className={styles.iconDistance} />Contact</Link>
-                    <Link to='/About'><FaInfo className={styles.iconDistance} />About</Link>
-                  </div>
-                </div>
-              </nav>
-            )
-          }
-
+          <nav className={classNames} onClick={() => setMenuMobileOpen(!isMenuMobileOpen)}>
+            <div className={styles.wrap}>
+              <div className={'grid ' + styles.navbar} >
+                <Link to="/" className="current-page"><FaHome className={styles.iconDistance} />Home</Link>
+                <Link to="/Cardapio"><FaPizzaSlice className={styles.iconDistance} />Menu</Link>
+                <Link to="/Contact"><FaUsers className={styles.iconDistance} />Contact</Link>
+                <Link to='/About'><FaInfo className={styles.iconDistance} />About</Link>
+              </div>
+            </div>
+          </nav>
 
           <Link className={styles.logoContainer} to="/">
             <Image src={'/logo.png'} alt="Logo Pizzaria" objectFit="contain" layout="fill" className={styles.logo} />
           </Link>
 
-          <Button to={'https://api.whatsapp.com/send?phone=79992504020&text=Olá%20,%20Bem-vindo%20a%20melhor pizzaria%20do%20pais!!'} target="_blank" className={styles.button} style={{order: 2}}>
+          <Button to={'https://api.whatsapp.com/send?phone=79992504020&text=Olá%20,%20Bem-vindo%20a%20melhor pizzaria%20do%20pais!!'} target="_blank" className={styles.button} style={{ order: 2 }}>
             <FaWhatsapp />
             Contact
           </Button>
